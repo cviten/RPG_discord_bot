3f02f7e5231188ee794bbc12771c1aea6e3aa311
cviten <vitn3@yandex.ru>
2017-08-23 03:43:04 +0400

    List function fixes

--- commands/list.js
+++ commands/list.js
@@ -1,6 +1,6 @@
 exports.run = (client, message, args) => {
-  let list_str ="Something wrong if nothing else: ";
-  let l = client.commands.map(cmd => cmd.name).join("\n");
+  let list_str ="List of commands:\n";
+  let l = client.commands.map(cmd => `${cmd.help.name} \n  ${cmd.help.desc}`).join("\n");
   message.channel.send(list_str + l);
 }
 
